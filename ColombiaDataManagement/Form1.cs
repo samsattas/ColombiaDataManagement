72b5f111b47ca7993141e2a36c8afcb556cef1b7
Samuel Satizabal <samuelsatizabal@hotmail.com>
2020-08-20 17:21:31 -0500

    fix

--- ColombiaDataManagement/Form1.cs
+++ ColombiaDataManagement/Form1.cs
@@ -16,5 +16,28 @@ namespace ColombiaDataManagement
         {
             InitializeComponent();
         }
+
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(872, 253);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
     }
 }
